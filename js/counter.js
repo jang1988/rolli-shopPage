cfb11ed0e3d4df64ce11bd48bd281452a90c2e34
Baklay Petro <pustota1988@gmail.com>
2022-06-19 13:41:43 +0300

    master

--- js/counter.js
+++ js/counter.js
@@ -1,17 +1,9 @@
-// Находим элементы на странице
-const btnMinus = document.querySelector('[data-action="minus"]');
-const btnPlus = document.querySelector('[data-action="plus"]');
-const counter = document.querySelector("[data-counter]");
+// Добавляем прослушку на всем окне
 
-// Отслеживаем клик на кнопку минус
-btnMinus.addEventListener("click", function () {
-  // проверяем  чтобы счетчик быд больше 0
-  if (parseInt(counter.innerText) > 0) {
-    counter.innerText = --counter.innerText;
-  }
-});
+window.addEventListener('click', function () {
+  console.log('www');
 
-// Отслеживаем клик на кнопку плюс
-btnPlus.addEventListener("click", function () {
-  counter.innerText = ++counter.innerText;
+  console.log(event.target);
+
+  
 });
